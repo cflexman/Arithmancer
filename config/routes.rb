f97bdba5d8f47b0f789379c84d23995f12e7aaa5
cflexman <Connor_Flexman@alumni.brown.edu>
2017-04-05 14:26:26 -0700

    Finished static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-
-  get 'foo/baz'
-
+  root 'static_pages#home'
+	get 'static_pages/about'
   get 'static_pages/home'
-
   get 'static_pages/help'
-
-  resources :microposts
-  resources :users
-  root 'users#index'
 end
